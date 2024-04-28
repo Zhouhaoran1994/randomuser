b742c5ec53d27fa01158c1355c41ded0e9ad065c
Zhou HaoRan <524480724@qq.com>
2024-04-28 23:39:22 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
+const User = ({ firstName, lastName, dob, hkid }) => (
+  <div>
+    <p>First Name: {firstName}</p>
+    <p>Last Name: {lastName}</p>
+    <p>Date of Birth: {dob}</p>
+    <p>HKID: {hkid}</p>
+  </div>
+);
 function App() {
+  const userDetails = {
+    firstName: "Nah",
+    lastName: "Nah",
+    dob: "0000-00-00",
+    hkid: "0000"
+  };
+
+  var [user, setUser] = useState(userDetails);
+
+  function handleGenerate() {
+    fetch("https://randomuser.me/api/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let userFetchingDetails = {
+          firstName: data.results[0].name.first,
+          lastName: data.results[0].name.last,
+          dob: data.results[0].dob.date,
+          hkid: data.results[0].id.value,
+        }
+        setUser(userFetchingDetails);
+      });
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random User Generator</h1>
+      <User {...user} />
+      <button onClick={handleGenerate}>Generate</button>
+      <button>Copy</button>
     </div>
   );
 }
